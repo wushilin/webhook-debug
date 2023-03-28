ddd33062cf30aa7ae64b93bfca4de2be0928031d
Shilin Wu <shwu@confluent.io>
2023-03-28 15:44:59 +0800

    Initial

--- src/main/kotlin/net/wushilin/webhookdebug/WebService.kt
+++ src/main/kotlin/net/wushilin/webhookdebug/WebService.kt
@@ -2,6 +2,7 @@ package net.wushilin.webhookdebug
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import java.io.FileOutputStream
@@ -16,6 +17,7 @@ class WebService {
         val baseFolder = getBase()
         val sequence = AtomicLong(0L);
         val randStart = Random().nextLong() % 10000;
+        val logger = LoggerFactory.getLogger(WebService.javaClass)
         fun getBase():java.io.File {
             var baseFolder = System.getProperty("dump.dest.folder")
             if (baseFolder == null || baseFolder.trim().isEmpty()) {
@@ -48,20 +50,25 @@ class WebService {
 
     @RequestMapping("/*")
     fun doWork(req: HttpServletRequest, resp: HttpServletResponse) {
-        val now = Date()
-        val destFile = java.io.File(baseFolder, formatSuffix(now))
-        val parentFile = destFile.parentFile
-        if(!parentFile.exists() ) {
-            parentFile.mkdirs()
-        }
-        if(!parentFile.exists()) {
-            resp.writer.write("Can't write the file. sorry")
-            return
-        }
-        val os = FileOutputStream(destFile)
-        os.use {
-            Request(req).dumpTo(it)
-            resp.writer.write("Thanks. It was saved.")
+        logger.info("${req.method} ${req.requestURL} BEGIN")
+        try {
+            val now = Date()
+            val destFile = java.io.File(baseFolder, formatSuffix(now))
+            val parentFile = destFile.parentFile
+            if (!parentFile.exists()) {
+                parentFile.mkdirs()
+            }
+            if (!parentFile.exists()) {
+                resp.writer.write("Can't write the file. sorry")
+                return
+            }
+            val os = FileOutputStream(destFile)
+            os.use {
+                Request(req).dumpTo(it)
+                resp.writer.write("Thanks. It was saved.")
+            }
+        } finally {
+            logger.info("${req.method} ${req.requestURL} DONE")
         }
     }
 }
