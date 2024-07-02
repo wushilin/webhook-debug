9a1e3d50fbf2fe1cf8d7edee23af369f09bae0dc
Wu Shilin <wushilin.sg@gmail.com>
2024-07-02 11:42:00 +0000

    Revise build script

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 
 java \
-      -Dserver.address=192.168.44.100 \
+      -Dserver.address=192.168.44.30 \
       -Dserver.port=4747 \
       -Ddump.dest.folder=/nfs/services/webhook-data \
       -jar webhook-debug-1.0.3-release.jar \
